b1f4cf26569496ba45b5449f3978e506f1e91c68
KirillMotov <kirill.m.work.afl@gmail.com>
2025-03-27 22:53:47 +0300

    Update AllScripts.lua

--- AllScripts.lua
+++ AllScripts.lua
@@ -1,6 +1,6 @@
 -- First
 loadstring(game:HttpGet("https://raw.githubusercontent.com/daniivanovivanov/prosharka/refs/heads/main/forqv0ckandmotov"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KirillMotov/Presents/refs/heads/main/Fling?token=GHSAT0AAAAAADBH5MMFXIM27NWEKMAPZ2MOZ7FVTDQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KirillMotov/Presents/refs/heads/main/Fling?token=GHSAT0AAAAAADBH5MME43G7QQFEFFEPIVLOZ7FVT3A"))()
 
 -- Second
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
